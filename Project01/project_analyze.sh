5fbac02d7a69da6e63bdd7fc6b064d3d985deb5b
sw-1oeo <sunwooj@mcmaster.ca>
2019-02-26 23:42:43 +0000

    featrue 3 done

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "select your feature:\n1:Create a TODO Log\n2:Compile Error Log\n3:Merge Log\n4:file type count\n5:delete temporary files\n6:find last working file\n"
+printf "select your feature:\n1:Create a TODO Log\n2:Compile Error Log\n3:Custom Feature-change upper to lowercase in all txt files\n4:file type count\n5:delete temporary files\n"
 printf "your option(pick the number):"
 read jin 
 if [ $jin = "1" ];then
@@ -10,16 +10,13 @@ if [ $jin = "2" ];then
 	echo  not done yet, spared for part2
 fi
 if [ $jin = "3" ];then
-        echo not done yet, spared for part2 
+        sed 'y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/' *.txt > lowercase_txt.log
 fi
 if [ $jin = "4" ];then
         echo not done yet, spared for part2 
 fi
 if [ $jin = "5" ];then
         echo git ls-files --others --exclude-standard | grep -E .tmp | xargs rm 
-fi
-if [ $jin = "6" ];then
-        echo  hi 
 fi
 
 
