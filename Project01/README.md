e372fea71689923c5176a8af2d3e28e44dae0eef
sw-1oeo <sunwooj@mcmaster.ca>
2019-02-27 04:38:43 +0000

    part2 completed

--- Project01/README.md
+++ Project01/README.md
@@ -1,8 +1,16 @@
-
-feature 5.1: script input
+Feature 5.1: script input
 
 Once the script is executed, it first shows options of features.
 As the user input their option of number, this input is stored to variable named jin.
 If statements are used here, so, for example, if the user inputted 1, variable jin is matched with 1, so by the if statement, it will work on the feature 5.2 which is to create a TODO log using grep.   
 
-I left other features (from 2 to 6) for project1-part2 for now.
+Custom feature 1(option 2): 
+It tries to compile every python file in the repo using for loop and with the command “python -m py_compile”, and it redirect the standard error to python_error.log.
+Therefore, you can view all the error message in the “python_error.log” 
+ 
+Custom feature 2(option 3):
+Using sed command, this feature changes all the uppercase to lowercase in any text file that are in the repo. The result of this is redirected(with >, which also means 1>) to “lowercase_txt.log” here. You can view all the text file contents with lowercases here.
+
+Custom feature 3(option 6):
+It tries to compile every Haskell file in the repo using for loop and with the command “runhaskell”, and it redirect the standard error to haskell_error.log.
+Therefore, you can view all the error message in the “haskell_error.log”
--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-printf "select your feature:\n1:Create a TODO Log\n2:Custom Feature-error log for python files\n3:Custom Feature-change upper to lowercase in all txt files\n4:file type count\n5:delete temporary files\n"
+printf "select your feature:\n1:Create a TODO Log(5.2)\n2:Custom Feature-error log for python files\n3:Custom Feature-change upper to lowercase in all txt files\n4:File type count(5.5)\n5:Delete temporary files(5.6)\n6:Custom feature-error log for haskell files\n7:Compile error log(5.3)\n"
 printf "your option(pick the number):"
 read jin 
 if [ $jin = "1" ];then
 	grep --exclude=todo.log -r '#TODO' . > todo.log
 fi
 if [ $jin = "2" ];then
-	python -m py_compile *.py 2> python_error.log
+	for i in *.py; do python -m py_compile $i; done 2> python_error.log
 fi
 if [ $jin = "3" ];then
         sed 'y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/' *.txt > lowercase_txt.log
@@ -33,6 +33,15 @@ if [ $jin = "4" ];then
 fi
 if [ $jin = "5" ];then
         echo git ls-files --others --exclude-standard | grep -E .tmp | xargs rm 
+fi
+if [ $jin = "6" ];then
+        for i in *.hs; do runhaskell $i; done 2> haskell_error.log
+fi
+if [ $jin = "7" ];then
+        for i in *.py; do python -m py_compile $i; done 2>> error_python.log
+	cat error_python.log | grep -r 'File' >> compile_fail.log 
+	for i in *.hs; do runhaskell $i; done 2>> error_haskell.log
+	cat error_haskell.log | grep -r ': error:' >> compile_fail.log
 fi
 
 
