4f51a60383bb198a1350d4746281b3424e3188cc
sw-1oeo <sunwooj@mcmaster.ca>
2019-04-18 18:21:09 +0000

    added accouts app

--- Project03/django_project/django_project/settings.py
+++ Project03/django_project/django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/e/sunwooj/static/'
