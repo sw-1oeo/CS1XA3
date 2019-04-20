85f729ada934b0c5350b2ce8b51fb6cf434031f5
sw-1oeo <sunwooj@mcmaster.ca>
2019-04-20 00:26:42 +0000

    added userprofile to store max score

--- Project03/django_project/accounts/models.py
+++ Project03/django_project/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    max_score = models.IntegerField()
+    
+    def __str__(self):
+        return "{} {}".format(self.user.username,self.max_score)
+
