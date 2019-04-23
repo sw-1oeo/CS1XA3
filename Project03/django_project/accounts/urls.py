11f69c49e78e28ee542d9e79777ba9b2b956b278
sw-1oeo <sunwooj@mcmaster.ca>
2019-04-23 19:27:22 +0000

    wrote register_user_view function in views.py and hooked it up to urls.py

--- Project03/django_project/accounts/urls.py
+++ Project03/django_project/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('accounts/login/', views.login_user_view , name='login_user'),
     path('accounts/logout/', views.logout_user_view , name='logout_user'),
+    path('accounts/user/', views.register_user_view , name='register_user'),
 ]
 
 
--- Project03/django_project/accounts/views.py
+++ Project03/django_project/accounts/views.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from accounts.models import UserProfile
+
 
 def login_user_view(request):
     print('request body', request.body)
@@ -30,5 +33,27 @@ def logout_user_view(request):
   status_code = 200
   message = "Logged out"
   return JsonResponse({'message': message}, status=status_code)
-  
 
+
+def register_user_view(request):
+    json_data = json.loads(request.body)
+    
+    username = json_data.get('username') 
+    password = json_data.get('password')   
+    confirm_password = json_data.get('confirm_password')
+
+
+    status_code = 400
+    if User.objects.filter(username=username).count() > 0:
+        message = "Username not available"
+
+    else:
+        message = "Passwords do not match"
+
+        if (password==confirm_password):
+            new_user = User.objects.create_user(username=username, password=password)
+            user_profile = UserProfile.objects.create(user=new_user, max_score=0)
+
+            status_code = 201
+            message = "User created"
+    return JsonResponse({'message': message}, status=status_code)
